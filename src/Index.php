6f475598987ad6da3d8c0d8246c8a12f6dadfba1
Georg Dobe <dobe@dc-solution.de>
2022-05-19 12:16:32 +0200

    Georg TEST

--- src/Index.php
+++ src/Index.php
@@ -1,4 +1,15 @@
 <?php
+/*
+$dbh = new PDO('mysql:host=127.0.0.1;port=3406;dbname=2021portverein', 'sportverein', 'sportverein');
+// hier die Verbindung nutzen
+$sth = $dbh->query('SELECT * FROM mitglied');
+*/
+
+// und nachdem das erledigt ist, wird sie geschlossen
+$sth = null;
+$dbh = null;
+
+echo "test";
 echo 'test';
 echo 'test';
 echo 'test_ds';
