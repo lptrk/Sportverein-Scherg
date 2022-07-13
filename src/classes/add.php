34ad10ef613739ee13b928dc1c964a2a85e60536
lugie15 <davidluge575@gmail.com>
2022-07-13 11:34:32 +0200

    kleinen bug fixen

--- src/classes/add.php
+++ src/classes/add.php
@@ -3,7 +3,7 @@
 include "usermanagement.classes.php";
 
 if(isset($_POST["name"], $_POST["lastname"], $_POST["plz"], $_POST["ort"], $_POST["geschlecht"], $_POST["sportarten"])){
-        userManagement:insertMember($_POST["name"], $_POST["lastname"], $_POST["plz"], $_POST["ort"], $_POST["geschlecht"], $_POST["sportarten"]);
+        userManagement::insertMember($_POST["name"], $_POST["lastname"], $_POST["plz"], $_POST["ort"], $_POST["geschlecht"], $_POST["sportarten"]);
     }else{
         header("location: ../mitgliederverwaltung/mitgliederverwaltung.php?error=no_values_given");
         exit();
